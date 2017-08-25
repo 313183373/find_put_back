0bb2f058a719ba6c7935979191520eed52692d5a
313183373 <313183373@qq.com>
2017-08-25 16:49:38 +0800

    update

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 function find_put_back(arr,target) {
-    let cnt=0;
-    arr=arr.filter(value=>{
-        if(value===target){
-            cnt++;
-            return false;
-        }
-        return true;
-    });
-    while(cnt--){
-        arr.push(target);
+    let temp=[];
+    while(arr.some(value=>value===target)){
+        arr.splice(arr.indexOf(target),1);
+        temp.push(target);
     }
-    return arr;
+    return arr.concat(temp);
 }
 
 console.log(find_put_back([0,1,0,3,12],0));
